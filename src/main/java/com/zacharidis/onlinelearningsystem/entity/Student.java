657f131b081b82be7626c47e58e7ae1b4138d05a
Georgios Zacharidis <zacharidis@icloud.com>
2022-12-28 15:26:17 +0200

    Student class added

---
+++ src/main/java/com/zacharidis/onlinelearningsystem/entity/Student.java
@@ -0,0 +1,25 @@
+package com.zacharidis.onlinelearningsystem.entity;
+
+import java.util.Objects;
+
+public class Student {
+
+    private Long studentId ;
+    private String firstName;
+    private String lastName;
+    private String level ;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return studentId.equals(student.studentId) && Objects.equals(firstName, student.firstName) && Objects.equals(lastName, student.lastName) && Objects.equals(level, student.level);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(studentId, firstName, lastName, level);
+    }
+}
