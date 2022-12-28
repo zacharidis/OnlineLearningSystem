b4830edcbf7650c0e54481fcd56c9e09467f74ce
Georgios Zacharidis <zacharidis@icloud.com>
2022-12-28 15:27:45 +0200

    User class created

---
+++ src/main/java/com/zacharidis/onlinelearningsystem/entity/User.java
@@ -0,0 +1,22 @@
+package com.zacharidis.onlinelearningsystem.entity;
+
+import java.util.Objects;
+
+public class User {
+    private Long userId;
+    private String email;
+    private String password;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return userId.equals(user.userId) && Objects.equals(email, user.email) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, email, password);
+    }
+}
