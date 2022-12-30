c78629c8bcda316d8d35376b783940e7bef76c2f
Georgios Zacharidis <zacharidis@icloud.com>
2022-12-30 17:03:25 +0200

    Role class finalized

--- src/main/java/com/zacharidis/onlinelearningsystem/entity/Role.java
+++ src/main/java/com/zacharidis/onlinelearningsystem/entity/Role.java
@@ -1,10 +1,45 @@
 package com.zacharidis.onlinelearningsystem.entity;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 public class Role {
     private Long roleId;
     private String name;
+
+    private Set<User> users = new HashSet<>();
+
+    public Role() {
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -17,5 +52,13 @@ public class Role {
     @Override
     public int hashCode() {
         return Objects.hash(roleId, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "roleId=" + roleId +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
