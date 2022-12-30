306524a6e987d2faaa1eafa78c84162ea64868c7
Georgios Zacharidis <zacharidis@icloud.com>
2022-12-30 16:59:30 +0200

    Role class finalized

--- src/main/java/com/zacharidis/onlinelearningsystem/entity/User.java
+++ src/main/java/com/zacharidis/onlinelearningsystem/entity/User.java
@@ -1,11 +1,37 @@
 package com.zacharidis.onlinelearningsystem.entity;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 public class User {
     private Long userId;
     private String email;
     private String password;
+
+    private Set<Role> roles = new HashSet<>();
+
+    private Student  student ;
+
+    private Instructor instructor;
+
+    public User() {
+    }
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -18,5 +44,53 @@ public class User {
     @Override
     public int hashCode() {
         return Objects.hash(userId, email, password);
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
     }
 }
