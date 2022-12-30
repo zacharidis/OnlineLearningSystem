610ce670ac337101ed274d3bffa48fddbe09338d
Georgios Zacharidis <zacharidis@icloud.com>
2022-12-30 16:56:22 +0200

    Student class finalized

--- src/main/java/com/zacharidis/onlinelearningsystem/entity/Student.java
+++ src/main/java/com/zacharidis/onlinelearningsystem/entity/Student.java
@@ -1,6 +1,8 @@
 package com.zacharidis.onlinelearningsystem.entity;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 public class Student {
 
@@ -9,6 +11,78 @@ public class Student {
     private String lastName;
     private String level ;
 
+    private Set<Course> courses = new HashSet<>();
+
+    private User user;
+
+
+    public Student() {
+    }
+
+    public Student(String firstName, String lastName, String level, User user) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.level = level;
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentId=" + studentId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", level='" + level + '\'' +
+                '}';
+    }
+
+    public Long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Long studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public Set<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(Set<Course> courses) {
+        this.courses = courses;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 
     @Override
     public boolean equals(Object o) {
