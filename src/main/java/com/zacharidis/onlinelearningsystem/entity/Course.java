14f4fdd9e3b862e85d4f7d55020577a15c51d17b
Georgios Zacharidis <zacharidis@icloud.com>
2022-12-30 16:51:13 +0200

    Course class finalized

--- src/main/java/com/zacharidis/onlinelearningsystem/entity/Course.java
+++ src/main/java/com/zacharidis/onlinelearningsystem/entity/Course.java
@@ -1,13 +1,90 @@
 package com.zacharidis.onlinelearningsystem.entity;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 public class Course {
+
+    public Course() {
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "courseId=" + courseId +
+                ", courseName='" + courseName + '\'' +
+                ", courseDuration='" + courseDuration + '\'' +
+                ", courseDescription='" + courseDescription + '\'' +
+                '}';
+    }
+
+    public Course(String courseName, String courseDuration, String courseDescription, Instructor instructor) {
+        this.courseName = courseName;
+        this.courseDuration = courseDuration;
+        this.courseDescription = courseDescription;
+        this.instructor = instructor;
+    }
+
+    public Long getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(Long courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseDuration() {
+        return courseDuration;
+    }
+
+    public void setCourseDuration(String courseDuration) {
+        this.courseDuration = courseDuration;
+    }
+
+    public String getCourseDescription() {
+        return courseDescription;
+    }
+
+    public void setCourseDescription(String courseDescription) {
+        this.courseDescription = courseDescription;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    public Set<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(Set<Student> students) {
+        this.students = students;
+    }
+
     private Long courseId;
     // non-nullable
     private String courseName;
     private String courseDuration;
     private String courseDescription;
+
+    // can be taught by 1 instructor
+    private Instructor instructor;
+
+    private Set<Student> students = new HashSet<>();
+
 
     @Override
     public boolean equals(Object o) {
