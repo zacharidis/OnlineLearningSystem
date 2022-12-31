67353e7c865617363365ad2bf5e9f39ecc048fd6
Georgios Zacharidis <zacharidis@icloud.com>
2022-12-31 22:04:47 +0200

    ORM annotations improved

--- src/main/java/com/zacharidis/onlinelearningsystem/entity/Role.java
+++ src/main/java/com/zacharidis/onlinelearningsystem/entity/Role.java
@@ -1,13 +1,26 @@
 package com.zacharidis.onlinelearningsystem.entity;
 
+import jakarta.persistence.*;
+
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+@Entity
+@Table(name="roles")
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="role_id",nullable = false)
     private Long roleId;
+
+    @Basic
+    @Column(name="name",nullable = false,length = 45,unique = true)
     private String name;
 
+
+    @ManyToMany(mappedBy = "roles",fetch = FetchType.LAZY)
     private Set<User> users = new HashSet<>();
 
     public Role() {
--- src/main/java/com/zacharidis/onlinelearningsystem/entity/User.java
+++ src/main/java/com/zacharidis/onlinelearningsystem/entity/User.java
@@ -1,18 +1,40 @@
 package com.zacharidis.onlinelearningsystem.entity;
 
+import jakarta.persistence.*;
+
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+
+@Entity
+@Table(name="users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="user_id",nullable = false)
     private Long userId;
+
+    @Basic
+    @Column(name = "email",nullable = false,length = 45,unique = true)
     private String email;
+
+
+    @Basic
+    @Column(name="password",nullable = false,length = 64)
     private String password;
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role",joinColumns = {@JoinColumn(name="user_id")},
+    inverseJoinColumns = {@JoinColumn(name="role_id")})
     private Set<Role> roles = new HashSet<>();
 
+
+    @OneToOne(mappedBy = "user")
     private Student  student ;
 
+    @OneToOne(mappedBy = "user")
     private Instructor instructor;
 
     public User() {
