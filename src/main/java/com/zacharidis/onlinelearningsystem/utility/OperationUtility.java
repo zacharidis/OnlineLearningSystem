b4b2e428db0432be20521b857ea11e698af9b0b5
Georgios Zacharidis <zacharidis@icloud.com>
2023-01-01 20:42:39 +0200

    Operation Utility helper  methods added

---
+++ src/main/java/com/zacharidis/onlinelearningsystem/utility/OperationUtility.java
@@ -0,0 +1,69 @@
+package com.zacharidis.onlinelearningsystem.utility;
+
+import com.zacharidis.onlinelearningsystem.dao.UserDao;
+import com.zacharidis.onlinelearningsystem.entity.User;
+import jakarta.persistence.EntityNotFoundException;
+
+public class OperationUtility {
+
+
+    // CRUD for users
+    public static void usersOperations(UserDao userDao){
+        createUsers(userDao);
+        updateUser(userDao);
+        deleteUser(userDao);
+        fetchUsers(userDao);
+
+
+    }
+
+    private static void fetchUsers(UserDao userDao) {
+
+        userDao.findAll().forEach(user-> System.out.println(user.toString()));
+
+
+
+    }
+
+    private static void deleteUser(UserDao userDao) {
+
+        User user = userDao.findById(3L).orElseThrow(()-> new EntityNotFoundException("User not found!"));
+        userDao.delete(user);
+
+
+
+
+    }
+
+    private static void updateUser(UserDao userDao) {
+
+
+        User user = userDao.findById(2L).orElseThrow(()-> new EntityNotFoundException("User not found!"));
+        user.setEmail("newEmail@gmail.com");
+        userDao.save(user);
+
+
+
+
+    }
+
+    private static void createUsers(UserDao userDao) {
+        User user1 = new User("user1@gmail.com","root");
+        userDao.save(user1);
+
+        User user2 = new User("user2@gmail.com","root");
+        userDao.save(user2);
+
+        User user3 = new User("user3@gmail.com","root");
+
+        userDao.save(user3);
+
+        User user4 = new User("user4@gmail.com","root");
+
+        userDao.save(user4);
+
+
+    }
+
+
+}
