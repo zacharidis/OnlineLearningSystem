2bd08b842cc8875e7212472d9c70a1d5b03a2a67
Georgios Zacharidis <zacharidis@icloud.com>
2023-01-01 20:10:22 +0200

    InstructorDao added

---
+++ src/main/java/com/zacharidis/onlinelearningsystem/dao/InstructorDao.java
@@ -0,0 +1,21 @@
+package com.zacharidis.onlinelearningsystem.dao;
+
+import com.zacharidis.onlinelearningsystem.entity.Instructor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface InstructorDao extends JpaRepository<Instructor,Long> {
+
+    @Query(value="select i from Instructor as i where  i.firstName like %:name% or i.lastName like %:name%")
+    List<Instructor> findInstructorsByName(@Param("name")String name);
+
+
+    @Query(value = "select i from Instructor  as i where i.user.email =:email")
+    Instructor findInstructorsByEmail (@Param("email") String email);
+
+
+
+}
